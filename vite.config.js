75be7add4deebf8e134e1259fa082ae1a652d37c
“Marie <marie@wastedeffort.ca>
2025-06-08 21:43:55 -0400

    vite config again

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    target: "es2022",
+  },
+  esbuild: {
+    target: "es2022",
+  },
   optimizeDeps: {
     esbuildOptions: {
-      target: "esnext",
+      target: "es2022",
     },
   },
 });
