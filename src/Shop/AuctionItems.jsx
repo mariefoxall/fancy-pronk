9b2766aaa3f33f8ff4b0dead40adaaba4662c510
“Marie <marie@wastedeffort.ca>
2025-06-15 13:59:00 -0400

    the rest of the auction items & responsive for mobile!

--- src/Shop/AuctionItems.jsx
+++ src/Shop/AuctionItems.jsx
@@ -1,7 +1,3 @@
-import bestinShowCups from "../assets/best-in-show-cups.jpg";
-import bestinShowTrophy from "../assets/best-in-show-trophy.jpg";
-import ceram from "../assets/ceram-corner.jpg";
-import lampMaze from "../assets/chainmail-lamp-bead-maze.jpg";
 import mermaid1 from "../assets/mermaid.jpg";
 import mermaid2 from "../assets/mermaid-2.jpg";
 import mermaid3 from "../assets/mermaid-3.jpg";
@@ -12,6 +8,34 @@ import betty4 from "../assets/betty-5.jpg";
 import lemon1 from "../assets/lemon-peeled.jpg";
 import lemon2 from "../assets/lemon-peeled-2.jpg";
 import lemon3 from "../assets/lemon-peeled-3.jpg";
+import term1 from "../assets/term.jpg";
+import term2 from "../assets/term-2.jpg";
+import term3 from "../assets/term-3.jpg";
+import term4 from "../assets/term-4.jpg";
+import metal1 from "../assets/metal.jpg";
+import metal2 from "../assets/metal-2.jpg";
+import metal3 from "../assets/metal-3.jpg";
+import oyster1 from "../assets/oyster.jpg";
+import oyster2 from "../assets/oyster-2.jpg";
+import oyster3 from "../assets/oyster-3.jpg";
+import oyster4 from "../assets/oyster-full.jpg";
+import oyster5 from "../assets/oyster-full2.jpg";
+import oyster6 from "../assets/oyster-full-3.jpg";
+import pomegranate1 from "../assets/pomegranate.jpg";
+import pomegranate2 from "../assets/pomegranate-2.jpg";
+import boot1 from "../assets/boot.jpg";
+import boot2 from "../assets/boot-2.jpg";
+import boot3 from "../assets/boot-3.jpg";
+import boot4 from "../assets/boot-4.jpg";
+import boot5 from "../assets/boot-5.jpg";
+import boot6 from "../assets/boot-6.jpg";
+import chainKeys1 from "../assets/chain-keys.jpg";
+import chainKeys2 from "../assets/chain-keys-2.jpg";
+import grapes1 from "../assets/grapes.jpg";
+import grapes2 from "../assets/grapes-2.jpg";
+import matches1 from "../assets/matches.jpg";
+import matches2 from "../assets/matches-2.jpg";
+import matches3 from "../assets/matches-3.jpg";
 
 import { getBidsForItem } from "../firebase";
 
@@ -19,7 +43,7 @@ const littleMermaid = {
   currentBid: 425,
   currentWinner: "",
   incrementValue: 10,
-  itemId: "little-mermaid",
+  itemId: "the-little-mermaid",
   itemName: "The Little Mermaid",
   description:
     "A true to life sized representation of the white plastic VHS covers that Disney used to have in the nineties. Porcelain and one billion glazes, some rare earth colour changing glazes. Cone 6 Oxidation.",
@@ -28,7 +52,7 @@ const littleMermaid = {
 };
 
 const tunnel = {
-  currentBid: 425,
+  currentBid: 450,
   incrementValue: 10,
   itemId: "tunnel-of-love",
   itemName: "The Tunnel of Love",
@@ -50,8 +74,105 @@ const peeledLemon = {
   imgSrc: [lemon1, lemon2, lemon3],
   altText: "Porcelain sculpture of a peeled lemon",
 };
+const terminator = {
+  currentBid: 450,
+  incrementValue: 10,
+  itemId: "terminator",
+  itemName: "Terminator VHS",
+  description:
+    "A true to life sized representation of Terminator on VHS, double sided. Porcelain and one billion glazes, some rare earth colour changing glazes. Cone 6 Oxidation. ",
+  imgSrc: [term4, term1, term2, term3],
+  altText: "Porcelain rendition of the classic Terminator film on VHS",
+};
 
-export const allAuctionitems = [littleMermaid, tunnel, peeledLemon];
+const heavyMetal = {
+  currentBid: 375,
+  incrementValue: 10,
+  itemId: "heavy-metal",
+  itemName: "Heavy Metal VHS",
+  description:
+    "A true to life sized representation of Heavy Metal on VHS. Porcelain and one billion glazes. Cone 6 Oxidation. ",
+  imgSrc: [metal1, metal2, metal3],
+  altText: "Porcelain rendition of the VHS film case of Heavy Metal",
+};
+
+const oyster = {
+  currentBid: 300,
+  incrementValue: 10,
+  itemId: "oyster",
+  itemName: "The Extremely Gay Oyster",
+  description:
+    "A life sized BC Oyster (BC because it's pretty big). This piece was atmospherically fired in the gas reduction salt kiln at Medalta in Medicine Hat Alberta during a year-long artist residency from September 2024 to present. This oyster has no glaze and was made using nerikomi techniques with coloured clay. The firing process makes a glazed surface with singular results. Porcelain, stains and oxides. Reduction salt fired, cone 10. Sculptural nerikomi pieces, especially fired in the atmospheric kilns may have small cracks and imperfections that are to be embraced.",
+  imgSrc: [oyster5, oyster1, oyster2, oyster3, oyster4, oyster6],
+  altText: "life-sized porcelain oyster with nerikomi detailing",
+};
+const pomegranate = {
+  currentBid: 475,
+  incrementValue: 10,
+  itemId: "pomegranate",
+  itemName: "Pomegranate",
+  description:
+    "A life sized Pomegranate half. This piece was atmospherically fired in the gas reduction salt kiln at Medalta in Medicine Hat Alberta during a year-long artist residency from September 2024 to present. This piece has no glaze and was made using nerikomi techniques with coloured clay. The firing process makes a glazed surface with singular results. Porcelain, stains and oxides. Reduction salt fired, cone 10. Sculptural nerikomi pieces, especially fired in the atmospheric kilns may have small cracks and imperfections that are to be embraced. ",
+  imgSrc: [pomegranate1, pomegranate2],
+  altText: "life-sized porcelain half-pomegranate with nerikomi detailing",
+};
+const boot = {
+  currentBid: 475,
+  incrementValue: 10,
+  itemId: "boot",
+  itemName: "The Boot",
+  description:
+    "A size 8 nerikomi Doc Martin boot. This piece was atmospherically fired in the gas reduction salt kiln at Medalta in Medicine Hat Alberta during a year-long artist residency from September 2024 to present. This boot has no glaze and was made using repeating pattern nerikomi cane with coloured clay. The firing process makes a glazed surface with singular results. Porcelaneous stoneware, stains and oxides. Reduction salt fired, cone 10. Sculptural nerikomi pieces, especially fired in the atmospheric kilns may have small cracks and imperfections that are to be embraced. This piece specifically has a large crack (shown in photos) that is mostly on the bottom of the piece and has been priced accordingly.",
+  imgSrc: [boot1, boot2, boot3, boot4, boot5, boot6],
+  altText: "Life-size porcelain Doc Marten boot with nerikomi detailing",
+};
+const chainKeys = {
+  currentBid: 275,
+  incrementValue: 10,
+  itemId: "chain-keys",
+  itemName: "Chain Keys",
+  description:
+    'A key chain with a 14" chain and bull clip. There are two house keys and one bike lock key. This piece was atmospherically fired in the gas reduction salt kiln at Medalta in Medicine Hat Alberta during a year-long artist residency from September 2024 to present. This keychain has no glaze and was made using nerikomi techniques with coloured clay. The firing process makes a glazed surface with singular results. Porcelain, stains and oxides. Reduction salt fired, cone 10. Sculptural nerikomi pieces, especially fired in the atmospheric kilns may have small cracks and imperfections that are to be embraced.',
+  imgSrc: [chainKeys1, chainKeys2],
+  altText:
+    'Porcelain key chain with 14" chain, bull clip, two house keys and one bike lock key',
+};
+
+const grapes = {
+  currentBid: 500,
+  incrementValue: 10,
+  itemId: "grapes",
+  itemName: "Grapes",
+  description:
+    'A large bunch of grapes approx 8"x5"x5". This piece was atmospherically fired in the gas reduction salt kiln at Medalta in Medicine Hat Alberta during a year-long artist residency from September 2024 to present. These stripey grapes have no glaze and were made using nerikomi techniques with coloured clay. The firing process makes a glazed surface with singular results. Porcelain, stains and oxides. Reduction salt fired, cone 10. Sculptural nerikomi pieces, especially fired in the atmospheric kilns may have small cracks and imperfections that are to be embraced. ',
+  imgSrc: [grapes1, grapes2],
+  altText: "Porcelain bunch of grapes with nerikomi detailing",
+};
+
+const matches = {
+  currentBid: 175,
+  incrementValue: 10,
+  itemId: "matches",
+  itemName: "Book of Matches",
+  description:
+    "A life sized book of matches using nerikomi cane pattern and reactive colour with tiny matches inside. This piece was atmospherically fired in the gas reduction salt kiln at Medalta in Medicine Hat Alberta during a year-long artist residency from September 2024 to present. This matchbook has no glaze and was made using nerikomi techniques with coloured clay. The firing process makes a glazed surface with singular results. Porcelain, stains and oxides. Reduction salt fired, cone 10. Sculptural nerikomi pieces, especially fired in the atmospheric kilns may have small cracks and imperfections that are to be embraced. ",
+  imgSrc: [matches1, matches2, matches3],
+  altText: "Porcelain life-sized book of matches with nerikomi detailing",
+};
+
+export const allAuctionitems = [
+  littleMermaid,
+  tunnel,
+  peeledLemon,
+  terminator,
+  heavyMetal,
+  oyster,
+  pomegranate,
+  boot,
+  chainKeys,
+  grapes,
+  matches,
+];
 
 export const getAuctionItemDBInfo = async (auctionItem) => {
   const allBids = await getBidsForItem(auctionItem.itemId);
